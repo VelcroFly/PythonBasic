df879bbc97eaa58ec0f982b908cd1621111da5e7
AntonBash <bash_anton@mail.ru>
2020-12-13 16:00:03 +0300

    Задание из п.6

--- lesson 01/2.py
+++ lesson 01/2.py
@@ -3,6 +3,4 @@ hours = seconds // 3600
 minutes = seconds // 60 % 60
 secs = seconds % 60
 
-# print(f'Вереводим {seconds} секунд в формат чч:мм:сс.')
-# print(f'Результат: {(seconds // 3600):02}:{(seconds % 3600 // 60):02}:{(seconds % 3600 // 60):02}')
 print(f'{hours:02}:{minutes:02}:{secs:02}')
