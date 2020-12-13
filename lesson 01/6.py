12e2360e39676823c6f9e81396ec811b5f1af9fd
AntonBash <bash_anton@mail.ru>
2020-12-13 15:58:33 +0300

    Задание из п.6

--- lesson 01/6.py
+++ lesson 01/6.py
@@ -3,7 +3,8 @@ b = float(input('Желаемый результат: '))
 day_counter = 1
 
 while a < b:
+    print(f'{day_counter}-й день - дистанция {a:.2f}')
     a += a * 0.1
     day_counter += 1
 
-print(day_counter)
+print(f'Через {day_counter} дней спортсмен достгнет результата не менее, чем {b} км')
