fef2863348433ce8634b8081305f9d846dea0c25
AntonBash <bash_anton@mail.ru>
2020-12-25 18:04:42 +0300

    Задание 5

--- lesson 04/5.py
+++ lesson 04/5.py
@@ -1,3 +1,6 @@
+from itertools import count, cycle
+from sys import argv
+
 """Реализовать два небольших скрипта:
 а) итератор, генерирующий целые числа, начиная с указанного,
 б) итератор, повторяющий элементы некоторого списка, определенного заранее.
@@ -6,4 +9,26 @@
 Например, в первом задании выводим целые числа, начиная с 3, а при достижении числа 10 завершаем цикл.
 Во втором также необходимо предусмотреть условие, при котором повторение элементов списка будет прекращено."""
 
-from itertools import count, cycle
+"""
+params: a 3 10
+params: b 10 10 35 21 58 63
+"""
+
+if argv[1] == 'a':
+    for i in count(int(argv[2])):
+        if i > int(argv[3]):
+            break
+        else:
+            print(i)
+elif argv[1] == 'b':
+    repeat_counter = 0
+    for i in cycle(argv[3:]):
+        repeat_counter += 1
+        if repeat_counter < int(argv[2]) * len(argv[3:]):
+            print(i)
+        else:
+            break
+else:
+    pass
+
+
