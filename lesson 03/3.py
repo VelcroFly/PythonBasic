8c30a6d4f6cfdb5a550e3ad7ee7694a7499580ee
AntonBash <bash_anton@mail.ru>
2020-12-25 14:13:52 +0300

    Добавлен вариант решения

--- lesson 03/3.py
+++ lesson 03/3.py
@@ -7,6 +7,15 @@ def my_func(a, b, c):
         return a + b
 
 
+def my_func_v2(a, b, c):
+    my_list = [a, b, c]
+    try:
+        my_list.remove((min(my_list)))
+        return sum(my_list)
+    except TypeError:
+        return TypeError
+
+
 if __name__ == '__main__':
     print(my_func(1, 2, 3))
     print(my_func(1, 3, 2))
@@ -14,4 +23,10 @@ if __name__ == '__main__':
     print(my_func(9, 4, 2))
     print(my_func(4, 7, 1))
     print(my_func(4, 1, 7))
+    print(my_func_v2(1, 2, 3))
+    print(my_func_v2(1, 3, 2))
+    print(my_func_v2(9, 2, 4))
+    print(my_func_v2(9, 4, 2))
+    print(my_func_v2(4, 7, 1))
+    print(my_func_v2(4, 1, 7))
 
