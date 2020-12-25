2b51b94ec9eb19801606d1d4e6ab91b727fb0a5f
AntonBash <bash_anton@mail.ru>
2020-12-25 18:13:50 +0300

    Устранена путанница в номерах

--- lesson 04/5.py
+++ lesson 04/5.py
@@ -1,34 +1,20 @@
-from itertools import count, cycle
-from sys import argv
-
-"""Реализовать два небольших скрипта:
-а) итератор, генерирующий целые числа, начиная с указанного,
-б) итератор, повторяющий элементы некоторого списка, определенного заранее.
-Подсказка: использовать функцию count() и cycle() модуля itertools. Обратите внимание,
-что создаваемый цикл не должен быть бесконечным. Необходимо предусмотреть условие его завершения.
-Например, в первом задании выводим целые числа, начиная с 3, а при достижении числа 10 завершаем цикл.
-Во втором также необходимо предусмотреть условие, при котором повторение элементов списка будет прекращено."""
-
+"""Реализовать формирование списка, используя функцию range() и возможности генератора. В список должны войти четные
+числа от 100 до 1000 (включая границы). Необходимо получить результат вычисления произведения всех элементов списка.
+Подсказка: использовать функцию reduce().
 """
-params: a 3 10
-params: b 10 10 35 21 58 63
-"""
-
-if argv[1] == 'a':
-    for i in count(int(argv[2])):
-        if i > int(argv[3]):
-            break
-        else:
-            print(i)
-elif argv[1] == 'b':
-    repeat_counter = 0
-    for i in cycle(argv[3:]):
-        repeat_counter += 1
-        if repeat_counter < int(argv[2]) * len(argv[3:]):
-            print(i)
-        else:
-            break
-else:
-    pass
+from functools import reduce
 
 
+def reducer_func(prev_element, current_element):
+    return prev_element * current_element
+
+
+values = [i for i in range(100, 1001) if i % 2 == 0]
+
+a = reduce(reducer_func, values)
+print(a)
+
+# values = [1, 2, 3, 1, 10]
+#
+# a = reduce(reducer_func, values)
+# print(a)
