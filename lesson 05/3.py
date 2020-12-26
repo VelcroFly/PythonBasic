0a2ae98ded9aff7223ea3c74acf50713a90f2d3f
AntonBash <bash_anton@mail.ru>
2020-12-26 13:16:24 +0300

    Задание 3

--- lesson 05/3.py
+++ lesson 05/3.py
@@ -5,12 +5,14 @@
 with open('employees.txt', 'r') as file:
     employees_counter = 0
     overall_salary = 0
+    salary_less_20000 = list()
 
     for line in file:
         name, salary = line.split(';')
         employees_counter += 1
         overall_salary += float(salary)
         if float(salary) < 20000:
-            print(name)
+            salary_less_20000.append(name)
 
     print(f'Средняя величина дохода: {(overall_salary / employees_counter):.2f}')
+    print(f'Сотрудники с окладом менее 20000: {salary_less_20000}')
